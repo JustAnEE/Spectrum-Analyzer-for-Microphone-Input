726aa3f8a2db7172d5161f5639f321bd08ea731d
Vincent Moroney <vmoroney@ualberta.ca>
2022-05-09 17:31:02 -0600

    Added functions for FFT and spectral output.

---
+++ src/spectrum_generation.cpp
@@ -0,0 +1,255 @@
+#include "spectral_fcns.h"
+
+
+
+void fft(fftw_complex* input, fftw_complex* output) {
+
+	// Creates FFT plan for computing forward DFT
+	// In the future this plan may change to a DCT plan for faster implementation, this may eliminate the need for shifting. 
+
+	fftw_plan plan = fftw_plan_dft_1d(buffer_size, input, output, FFTW_FORWARD, FFTW_ESTIMATE); 
+
+	fftw_execute(plan);
+	fftw_destroy_plan(plan);
+	fftw_cleanup();
+
+}
+
+
+//Not used for now.
+fftw_complex* set_fft(double *buffer) {
+
+	fftw_complex* y = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * (buffer_size));
+	fftw_complex* x = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * (buffer_size));
+
+	// Cannot directly pass buffer data into FFT, needs to be fftw_complex* data type.
+	for (int k = 0; k < buffer_size; k++) {
+
+		x[k][REAL] = buffer[k];
+		x[k][IMAG] = 0;
+
+	}
+	
+	// fft(x,y) will implicitly overwrite y
+	fft(x, y); 
+
+
+	return y; 
+
+}
+
+
+fftw_complex* better_set_fft(fftw_complex* shifted_input) {
+
+	fftw_complex* y = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * (buffer_size));
+
+
+	fft(shifted_input, y);
+
+
+	return y; 
+
+}
+
+
+
+// This function is most likely irrelevant for this application, though good to have. 
+void ifft(fftw_complex* in, fftw_complex* out){
+
+	fftw_plan plan = fftw_plan_dft_1d(buffer_size, in, out, FFTW_BACKWARD, FFTW_ESTIMATE); 
+	fftw_execute(plan);
+	fftw_destroy_plan(plan);
+	fftw_cleanup(); 
+
+	for (int k = 0; k <= buffer_size; k++) {
+
+		out[k][REAL] /= buffer_size;
+		out[k][IMAG] /= buffer_size; 
+
+	}
+
+}
+
+// Not used for now. 
+fftw_complex* fft_shift(fftw_complex* fft_data) {
+	
+	// Cursed function :) Probably not the best implementation of a fftshift functionality 
+
+	int half_size = buffer_size/2; 
+	fftw_complex* shifted_fft = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * (buffer_size));
+
+	for (int k = 0; k < buffer_size; k++) {
+
+		if (k < half_size) {
+
+			shifted_fft[k + half_size][REAL] = fft_data[k][REAL];
+			shifted_fft[k + half_size][IMAG] = fft_data[k][IMAG];
+
+		}
+
+
+		if (k >= half_size) {
+
+			shifted_fft[k - half_size][REAL] = fft_data[k][REAL];
+			shifted_fft[k - half_size][IMAG] = fft_data[k][IMAG];
+
+		}
+
+
+	}
+
+	return shifted_fft; 
+
+}
+
+fftw_complex* better_fft_shift(double* buffer) {
+
+	// Attempt at a better fftshift which preprocesses the input of the fft, instead of postprocessing fft output 
+
+	fftw_complex* x = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * (buffer_size ));
+
+	for (int k = 0; k < buffer_size; k++) {
+
+		// unreadable dsp wizardry :) 
+		x[k][REAL] = buffer[k]*pow(-1, k);
+		x[k][IMAG] = 0; 
+
+	}
+
+	return x; 
+
+}
+
+
+double* frequency_array(){
+
+	// Computes frequency for each FFT bin with the distance between each bin being sample_rate/buffer_size. 
+	// May need to increase buffer length to zero pad to an arbitrary power of 2 to interpolate the frequency response data. 
+
+	double fs = (double) sample_rate;
+	double N = (double) buffer_size;
+
+	double* frequency = (double*)calloc(buffer_size, sizeof(double));
+
+	for (int k = 0; k < buffer_size; k++) {
+
+		frequency[k] = - fs/2 + ((double)k)*fs/N;
+
+	}
+
+	return frequency; 
+
+}
+
+
+double* magnitude(fftw_complex* fft_data) {
+
+
+	double* magnitude_data = (double*)calloc(buffer_size, sizeof(double));
+	
+	for (int k = 0; k < buffer_size; k++) {
+
+		// This is most likely not the most hardware efficient way to do this :) 
+
+		double square = fft_data[k][REAL] * fft_data[k][REAL] + fft_data[k][IMAG] * fft_data[k][IMAG];
+
+		magnitude_data[k] = sqrt(square);
+
+	}
+
+	return magnitude_data;
+
+}
+
+
+double* dB_magnitude(double* magnitude_data) {
+
+	double* dB_magnitude_data = (double*)calloc(buffer_size, sizeof(double));
+
+	// Need to deal with magnitude data close to zero. Noise floor of -120 dB corresponds to a magnitude of 1e-6. 
+	double noise_floor = -120; 
+
+
+	for (int k = 0; k < buffer_size; k++) {
+
+		if (magnitude_data[k] < 1 * pow(10, -6) || (magnitude_data[k] == 0)) {
+
+			dB_magnitude_data[k] = noise_floor;
+
+		}
+
+		else {
+
+			dB_magnitude_data[k] = (double)20 * log10(magnitude_data[k]);
+
+		}
+
+	}
+
+	return dB_magnitude_data; 
+
+}
+
+
+// Same thing as dB_magnitude normalized to 1mW of power. 
+double* dBm_magnitude(double* magnitude_data) {
+
+	double* dBm_magnitude_data = (double*)calloc(buffer_size, sizeof(double));
+
+	// Need to deal with magnitude data close to zero. 
+	double noise_floor = -120;
+
+
+	for (int k = 0; k < buffer_size; k++) {
+
+		if (magnitude_data[k] < 1 * pow(10, -6) || (magnitude_data[k] == 0)){
+
+			dBm_magnitude_data[k] = noise_floor;
+
+		}
+
+		else {
+
+
+			dBm_magnitude_data[k] = (double)10 * log10(1000*magnitude_data[k]);
+		}
+
+	}
+	
+	return dBm_magnitude_data; 
+
+}
+
+
+double* power_spectral_density(double* magnitude_data) {
+
+	double* psd = (double*)calloc(buffer_size, sizeof(double));
+
+	for (int k = 0; k < buffer_size; k++) {
+
+		psd[k] = magnitude_data[k] / buffer_size;
+
+	}
+
+	return psd; 
+
+}
+
+
+// Organize data into the form {f_0, m_0, f_1, m_1, ... , f_k, m_k, .... }
+double* prep_data_for_plot(double* freqs, double* spectrum) {
+
+	int plot_data_size = 2 * buffer_size; 
+	double* data_for_plot = (double*)calloc(plot_data_size, sizeof(double));
+
+	for (int k = 0; k < plot_data_size; k+=2) {
+
+		int index = k / 2; 
+
+		data_for_plot[k] = freqs[index];
+		data_for_plot[k + 1] = spectrum[index];
+		
+	}
+
+	return data_for_plot; 
+}
---
+++ src/spectrum_generation.h
@@ -0,0 +1,55 @@
+#ifndef spectral_fcns_H
+#define spectral_fcns_H
+
+#include <fftw3.h>
+#include <math.h> 
+#include <iostream>
+
+
+
+#define REAL 1
+#define IMAG 0
+#define sample_rate 44100
+#define buffer_size 1024*8
+
+
+/* USING THESE FUNCTIONS:
+
+The ideal method of implementation as of now is as follows: The buffer is passed to the better_fft_shift function to preprocess the buffer such that the correct
+shifting occurs at FFT output. Then the shifted data is passed to the better_set_fft function to implement the FFT on the buffer data (fft is called in this
+function), and this data is in turn passed to the magnitude function to compute the complex modulus of each FFT element. Finally, frequency_array() is called
+to obtain the frequency data for each FFT magnitude bin. So each frequency[k] corresponds to magnitude[k].
+
+x axis for plotting: frequency array.
+y axis for plotting: magnitude array.
+
+The data is ready for plot via passing the frequency and magnitude (or other output) array to prep_data_for_plot.
+prep_data_for_plot returns a pointer to a double array with entries [freq_0, mag_0, freq_1, mag_1, ... ].
+
+Other:
+
+dB_magnitude returns the magnitude array according to the dB scale, i.e 20log_10(magnitude). This function has a default noise floor of -120dB.
+dBm_magnitude returns the same thing as dB_magnitude normalized to 1mW of power. Also has a default noise floor of -120dB
+power_spectral_density returns the PSD of the magnitude data (normalized magnitude data squared).
+
+NOTE: The standard .wav or PCM data sampling rate for audio applications is 44100Hz, this means the FFT analysis given by these functions captures the entire range
+of human hearing (-22050Hz, 22050Hz). In most audio applications, the audio present is contained within a much smaller frequency support, it may be necessary to only plot on
+fractions of the returned frequency range for a more coherent output.
+
+*/
+
+
+void fft(fftw_complex* input, fftw_complex* output);
+void ifft(fftw_complex* in, fftw_complex* out);
+fftw_complex* set_fft(double* buffer);
+fftw_complex* better_set_fft(fftw_complex* shifted_input);
+fftw_complex* fft_shift(fftw_complex* fft_data);
+fftw_complex* better_fft_shift(double* buffer);
+double* magnitude(fftw_complex* fft_data);
+double* dB_magnitude(double* magnitude_data);
+double* dBm_magnitude(double* magnitude_data);
+double* power_spectral_density(double* magnitude_data);
+double* frequency_array();
+double* prep_data_for_plot(double* freqs, double* spectrum);
+
+#endif // !spectral_fcns.h
